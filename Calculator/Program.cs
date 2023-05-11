d24890dfc0058b37c65ca981d7c08a327a0ffa15
kadiounas <kadi.ounas@tktk.ee>
2023-05-11 16:12:26 +0300

    add week7 code

---
+++ Calculator/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Enter the operation (+/-):");
+char userOperation = char.Parse(Console.ReadLine());
+Console.WriteLine("Enter a:");
+int firstNum = Int32.Parse(Console.ReadLine());
+Console.WriteLine("Enter b:");
+int secondNum = Int32.Parse(Console.ReadLine());
+
+switch (userOperation)
+{
+    case '+':
+        Addition(firstNum, secondNum);
+        break;
+    case '-':
+        Substraction(firstNum, secondNum);
+        break;
+    default:
+        Console.WriteLine("invalid operatior");
+        break;
+}
+static void Addition(int a, int b)
+{
+    Console.WriteLine($"{2} + {b} = {a + b}");
+}
+static void Substraction(int a, int b)
+{
+    Console.WriteLine($"{2} - {b} = {a - b}");
+}
---
+++ test/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Kas teisendan Fahrenheiti või Celsiusesse (F/C):");
+string userInput = Console.ReadLine().ToUpper();
+Console.WriteLine("Sisesta väärtus:");
+int value = Int32.Parse(Console.ReadLine());
+
+if(userInput =="F")
+{
+    ConvertToFarenheit(value);
+
+}
+else if(userInput == "C")
+{
+    ConvertToCelsius(value);
+}
+else
+{
+    Console.WriteLine("Proovi uuesti!");
+}
+
+static void ConvertToCelsius(int Value)
+{
+    Console.WriteLine($"{(Value - 32) * 5 / 9} kraadi");
+}
+static void ConvertToFarenheit(int Value)
+{
+    Console.WriteLine($"{(Value * 9) / 5 + 32} fahrenheiti");
+}
